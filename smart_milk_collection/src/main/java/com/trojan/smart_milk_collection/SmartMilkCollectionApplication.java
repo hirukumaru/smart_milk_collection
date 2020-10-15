ecde0d093d3f460d5db75617dc4aeffc91ba0e04
madushanka dissanayaka <72688429+hirukumaru@users.noreply.github.com>
2020-10-15 17:15:20 +0530

    new

---
+++ smart_milk_collection/src/main/java/com/trojan/smart_milk_collection/SmartMilkCollectionApplication.java
@@ -0,0 +1,15 @@
+package com.trojan.smart_milk_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMilkCollectionApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SmartMilkCollectionApplication.class, args);
+	}
+
+}
---
+++ smart_milk_collection/src/test/java/com/trojan/smart_milk_collection/SmartMilkCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trojan.smart_milk_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMilkCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
