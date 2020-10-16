bbc19bf10e57eb72228d92fab72da77e116a8c01
madushanka dissanayaka <72688429+hirukumaru@users.noreply.github.com>
2020-10-16 13:02:41 +0530

    innitialized

--- smart_milk_collection/src/main/java/com/trojan/smart_milk_collection/Controller/ComInfoController.java
+++ smart_milk_collection/src/main/java/com/trojan/smart_milk_collection/Controller/ComInfoController.java
@@ -1,3 +1,13 @@
+/**
+Author : Trojan Solution
+Email : trojansolution@gmail.com
+Web : www.trojansolution.com
+Developer : Madushanka Dissanayaka
+Developer Email : dmmdisa@gmail.com
+Description : File created on 2020-10-15 by Madushanka Dissanayaka.
+			  This is the controller class for company info.
+*/
+
 package com.trojan.smart_milk_collection.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
--- smart_milk_collection/src/main/java/com/trojan/smart_milk_collection/SmartMilkCollectionApplication.java
+++ smart_milk_collection/src/main/java/com/trojan/smart_milk_collection/SmartMilkCollectionApplication.java
@@ -1,3 +1,13 @@
+/**
+Author : Trojan Solution
+Email : trojansolution@gmail.com
+Web : www.trojansolution.com
+Developer : Madushanka Dissanayaka
+Developer Email : dmmdisa@gmail.com
+Description : project created on 2020-10-15 by Madushanka Dissanayaka.
+			  This is the main class of the project.
+*/
+
 package com.trojan.smart_milk_collection;
 
 import org.springframework.boot.SpringApplication;
